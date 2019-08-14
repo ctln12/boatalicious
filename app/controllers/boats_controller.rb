7f06e7554fb29ce6e81bdc701884dfb7d8f3d08c
James Hart <jhart2083@gmail.com>
2019-08-14 16:34:07 +0200

    Update seed to real ports of lac leman

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,6 +1,13 @@
 class BoatsController < ApplicationController
   def index
     @boats = Boat.all
+    @flats = Flat.geocoded #returns flats with coordinates
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
   end
 
   def show
