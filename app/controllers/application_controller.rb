d11183eddc55d5de6838802f5f7c215801a88e63
Cataline Baert <snuf_kin@yahoo.com>
2019-08-15 17:07:47 +0200

    add new method to import temporary variable to sign in/up window before redirecting to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    # redirect to the form if there is a form_data in the session
+    if session[:form_data].present?
+      # redirect to your form path
+      @booking = current_user.bookings.create(session[:form_data]["booking"])
+      # session[:form_data] = nil
+      flash[:notice] = "Sweet, logged in. Good choice btw!"
+      bookings_path(@booking.user, @booking)
+    else
+      # if there is not temp list in the session proceed as normal
+      super
+    end
+  end
 end
