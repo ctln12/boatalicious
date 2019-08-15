83be66b1b574997becda3cb3279c210c3917ea3f
James Hart <jhart2083@gmail.com>
2019-08-15 13:55:33 +0200

    added presence validations for all boat fields

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,11 @@ class Boat < ApplicationRecord
   belongs_to :user
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  validates :name, presence: true
+  validates :image_url, presence: true
+  validates :price_per_day, presence: true
+  validates :capacity, presence: true
+  validates :boat_type, presence: true
+  validates :description, presence: true
 end
